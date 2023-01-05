0b8a3f9099a5ddefbbc65cf8dd25611a1d8bb5ca
Олег Комисаренко <oleg.o.komisarenko@gmail.com>
2023-01-05 20:29:40 +0300

    createPoll method created

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
@@ -20,4 +22,40 @@ func main() {
 	http.HandleFunc("/api/poll/", makeVote)
 	http.HandleFunc("/api/getResult/", getResult)
 	log.Fatal(http.ListenAndServe(":8000", nil))
+}
+
+func createPoll(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	// Read the request body and unmarshal it into a Poll struct.
+	b, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Error reading request body", http.StatusBadRequest)
+		return
+	}
+	defer r.Body.Close()
+
+	var p Poll
+	err = json.Unmarshal(b, &p)
+	if err != nil {
+		http.Error(w, "Error parsing request body", http.StatusBadRequest)
+		return
+	}
+
+	// Assign the poll an ID and add it to the list of polls.
+	p.ID = len(polls) + 1
+	p.Results = make([]int, len(p.Choices))
+	polls = append(polls, p)
+
+	// Marshal the poll and write it to the response.
+	b, err = json.Marshal(p)
+	if err != nil {
+		http.Error(w, "Error marshalling response", http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(b)
 }
